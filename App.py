d6dfbd6c7e21dd60f046596feb31e2d04f1c23e3
Saamuu7 <samu05corrales@gmail.com>
2024-10-23 19:31:05 +0200

    Proyecto añadido

---
+++ App.py
@@ -0,0 +1,14 @@
+#%% 1. Introduccion
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 19:40:32 2024
+
+@author: Samuel
+"""
+
+
+#%% 2. Importar modulos
+import mysql.connector
+from mysql.connector import Error
+
+
---
+++ gestion_db.py
@@ -0,0 +1,17 @@
+#%% 1. Introduccion
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 18:17:50 2024
+
+@author: Samuel
+"""
+
+
+#%% 2. Importar modulos
+import csv
+import mysql.connector
+from gestion_db import conectar_bd
+
+
+
+#%% 3. Codigo
